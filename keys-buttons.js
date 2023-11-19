42bd58bf487bb3c806991092cf186925aaac348b
sarowley <sarowley@ucsc.edu>
2023-11-18 19:51:02 -0800

    finished :)

---
+++ keys-buttons.js
@@ -0,0 +1,599 @@
+//it runs, and in the end, isn't that all anyone ever is going for?
+
+//functions to change the keys being shown on the psuedo-keyboard
+function change1() {
+  //first switch
+  document.getElementById("a").value = "i";
+  document.getElementById("i").value = "a";
+}
+function change2() {
+  //changing back
+  document.getElementById("a").value = "a";
+  document.getElementById("i").value = "i";
+  //first switch
+  document.getElementById("t").value = "a";
+  document.getElementById("a").value = "h";
+  document.getElementById("h").value = "t";
+}
+
+function change3() {
+  //changing back
+  document.getElementById("a").value = "a";
+  document.getElementById("t").value = "t";
+  document.getElementById("h").value = "h";
+  //first change
+  document.getElementById("spacer1").value = " . ";
+  document.getElementById("spacer3").value = "space";
+}
+
+function change4() {
+  //changing back
+  document.getElementById("spacer1").value = "";
+  //first switch
+  document.getElementById("y").value = "is";
+  document.getElementById("s").value = "this";
+  document.getElementById("b").value = "cool?";
+}
+
+function change5() {
+  //changing back
+  document.getElementById("spacer3").value = "      ";
+  document.getElementById("y").value = "y";
+  document.getElementById("s").value = "s";
+  document.getElementById("b").value = "b";
+  //first switch
+  document.getElementById("a").value = "p";
+  document.getElementById("b").value = "g";
+  document.getElementById("c").value = "i";
+  document.getElementById("d").value = "f";
+  document.getElementById("e").value = "h";
+  document.getElementById("f").value = "w";
+  document.getElementById("g").value = "u";
+  document.getElementById("h").value = "q";
+  document.getElementById("i").value = "s";
+  document.getElementById("j").value = "k";
+  document.getElementById("k").value = "t";
+  document.getElementById("l").value = "l";
+  document.getElementById("m").value = "v";
+  document.getElementById("n").value = "e";
+  document.getElementById("o").value = "d";
+  document.getElementById("p").value = "a";
+  document.getElementById("q").value = "c";
+  document.getElementById("r").value = "j";
+  document.getElementById("s").value = "x";
+  document.getElementById("t").value = "b";
+  document.getElementById("u").value = "m";
+  document.getElementById("v").value = "o";
+  document.getElementById("w").value = "n";
+  document.getElementById("x").value = "y";
+  document.getElementById("y").value = "z";
+  document.getElementById("z").value = "r";
+}
+
+//this is where all the typing stuff is
+function typing(event) {
+  textbox.value = "";
+  textbox.value = textArray;
+
+  //backspace
+  if (event.keyCode == 8) {
+    if (textArray.length == 2) {
+      textArray = textArray.slice(0, 1);
+      textbox.value = textArray;
+    } else {
+      textArray = textArray.slice(0, -1);
+      textbox.value = textArray;
+    }
+  }
+
+  //space
+  if (event.keyCode == 32) {
+    textArray += " ";
+    textbox.value = textArray;
+  }
+
+  //comma
+  if (event.keyCode == 188) {
+    textArray += ",";
+    textbox.value = textArray;
+  }
+
+  //exclamation mark
+  if (event.keyCode == 49) {
+    textArray += "!";
+    textbox.value = textArray;
+  }
+
+  //apostrophe
+  if (event.keyCode == 222) {
+    textArray += "'";
+    textbox.value = textArray;
+  }
+
+  //period
+  if (event.keyCode == 190) {
+    textArray += ".";
+    textbox.value = textArray;
+  }
+
+  //a key
+  if (event.keyCode == 65) {
+    if (word1) {
+      textArray += "a";
+    }
+    if (word2) {
+      textArray += "i";
+    }
+    if (word3) {
+        textArray += "h";
+      }
+    if (word6) {
+      textArray += "p";
+    }
+    textbox.value = textArray;
+  }
+  //b key
+  if (event.keyCode == 66) {
+    if (word1 | word2 | word3) {
+      textArray += "b";
+    }
+    if (word6) {
+      textArray += "g";
+    }
+    textbox.value = textArray;
+  }
+  //c key
+  if (event.keyCode == 67) {
+    if (word1 | word2 | word3) {
+      textArray += "c";
+    }
+    if (word6) {
+      textArray += "i";
+    }
+    textbox.value = textArray;
+  }
+  //d key
+  if (event.keyCode == 68) {
+    if (word1 | word2 | word3) {
+      textArray += "d";
+    }
+    if (word6) {
+      textArray += "f";
+    }
+    textbox.value = textArray;
+  }
+  //e key
+  if (event.keyCode == 69) {
+    if (word1 | word2 | word3) {
+      textArray += "e";
+    }
+    if (word6) {
+      textArray += "h";
+    }
+    textbox.value = textArray;
+  }
+  //f key
+  if (event.keyCode == 70) {
+    if (word1 | word2 | word3) {
+      textArray += "f";
+    }
+    if (word6) {
+      textArray += "w";
+    }
+    textbox.value = textArray;
+  }
+  //g key
+  if (event.keyCode == 71) {
+    if (word1 | word2 | word3) {
+      textArray += "g";
+    }
+    if (word6) {
+      textArray += "u";
+    }
+    textbox.value = textArray;
+  }
+  //h key
+  if (event.keyCode == 72) {
+    if (word1  | word2) {
+      textArray += "h";
+    }
+    if (word3) {
+      textArray += "t";
+    }
+    if (word6) {
+      textArray += "q";
+    }
+    textbox.value = textArray;
+  }
+  //i key
+  if (event.keyCode == 73) {
+    if (word1 | word3) {
+      textArray += "i";
+    }
+    if (word2) {
+      textArray += "a";
+    }
+    if (word6) {
+      textArray += "s";
+    }
+    textbox.value = textArray;
+  }
+  //j key
+  if (event.keyCode == 74) {
+    if (word1 | word2 | word3) {
+      textArray += "j";
+    }
+    if (word6) {
+      textArray += "k";
+    }
+    textbox.value = textArray;
+  }
+  //k key
+  if (event.keyCode == 75) {
+    if (word1 | word2 | word3) {
+      textArray += "k";
+    }
+    if (word6) {
+      textArray += "t";
+    }
+    textbox.value = textArray;
+  }
+  //l key
+  if (event.keyCode == 76) {
+    if (word1 | word2 | word3 | word6) {
+      textArray += "l";
+    }
+    textbox.value = textArray;
+  }
+  //m key
+  if (event.keyCode == 77) {
+    if (word1 | word2 | word3) {
+      textArray += "m";
+    }
+    if (word6) {
+      textArray += "v";
+    }
+    textbox.value = textArray;
+  }
+  //n key
+  if (event.keyCode == 78) {
+    if (word1 | word2 | word3) {
+      textArray += "n";
+    }
+    if (word6) {
+      textArray += "e";
+    }
+    textbox.value = textArray;
+  }
+  //o key
+  if (event.keyCode == 79) {
+    if (word1 | word2 | word3) {
+      textArray += "o";
+    }
+    if (word6) {
+      textArray += "d";
+    }
+    textbox.value = textArray;
+  }
+  //p key
+  if (event.keyCode == 80) {
+    if (word1 | word2 | word3) {
+      textArray += "p";
+    }
+    if (word6) {
+      textArray += "a";
+    }
+    textbox.value = textArray;
+  }
+  //q key
+  if (event.keyCode == 81) {
+    if (word1 | word2 | word3) {
+      textArray += "q";
+    }
+    if (word6) {
+      textArray += "c";
+    }
+    textbox.value = textArray;
+  }
+  //r key
+  if (event.keyCode == 82) {
+    if (word1 | word2 | word3) {
+      textArray += "r";
+    }
+    if (word6) {
+      textArray += "j";
+    }
+    textbox.value = textArray;
+  }
+  //s key
+  if (event.keyCode == 83) {
+    if (word1 | word2 | word3) {
+      textArray += "s";
+    }
+    if (word6) {
+      textArray += "x";
+    }
+    textbox.value = textArray;
+  }
+  //t key
+  if (event.keyCode == 84) {
+    if (word1 | word2) {
+      textArray += "t";
+    }
+    if (word3) {
+      textArray += "a";
+    }
+    if (word6) {
+      textArray += "b";
+    }
+    textbox.value = textArray;
+  }
+  //u key
+  if (event.keyCode == 85) {
+    if (word1 | word2 | word3) {
+      textArray += "u";
+    }
+    if (word6) {
+      textArray += "m";
+    }
+    textbox.value = textArray;
+  }
+  //v key
+  if (event.keyCode == 86) {
+    if (word1 | word2 | word3) {
+      textArray += "v";
+    }
+    if (word6) {
+      textArray += "o";
+    }
+    textbox.value = textArray;
+  }
+  //w key
+  if (event.keyCode == 87) {
+    if (word1 | word2 | word3) {
+      textArray += "w";
+    }
+    if (word6) {
+      textArray += "n";
+    }
+    textbox.value = textArray;
+  }
+  //x key
+  if (event.keyCode == 88) {
+    if (word1 | word2 | word3) {
+      textArray += "x";
+    }
+    if (word6) {
+      textArray += "y";
+    }
+    textbox.value = textArray;
+  }
+  //y key
+  if (event.keyCode == 89) {
+    if (word1 | word2 | word3) {
+      textArray += "y";
+    }
+    if (word6) {
+      textArray += "z";
+    }
+    textbox.value = textArray;
+  }
+  //z key
+  if (event.keyCode == 90) {
+    if (word1 | word2 | word3) {
+      textArray += "z";
+    }
+    if (word6) {
+      textArray += "r";
+    }
+    textbox.value = textArray;
+  }
+}
+
+//top row
+q.addEventListener("click", qButton);
+w.addEventListener("click", wButton);
+e.addEventListener("click", eButton);
+r.addEventListener("click", rButton);
+t.addEventListener("click", tButton);
+y.addEventListener("click", yButton);
+u.addEventListener("click", uButton);
+i.addEventListener("click", iButton);
+o.addEventListener("click", oButton);
+p.addEventListener("click", pButton);
+
+//second row
+a.addEventListener("click", aButton);
+s.addEventListener("click", sButton);
+d.addEventListener("click", dButton);
+f.addEventListener("click", fButton);
+g.addEventListener("click", gButton);
+h.addEventListener("click", hButton);
+j.addEventListener("click", jButton);
+k.addEventListener("click", kButton);
+l.addEventListener("click", lButton);
+
+//bottom row
+z.addEventListener("click", zButton);
+x.addEventListener("click", xButton);
+c.addEventListener("click", cButton);
+v.addEventListener("click", vButton);
+b.addEventListener("click", bButton);
+n.addEventListener("click", nButton);
+m.addEventListener("click", mButton);
+
+//period & space
+spacer1.addEventListener("click", periodButton);
+spacer3.addEventListener("click", spaceButton);
+
+//top row
+function qButton() {
+  if (clicking) {
+    textbox.value += "q";
+  }
+}
+function wButton() {
+  if (clicking) {
+    textbox.value += "w";
+  }
+}
+function eButton() {
+  if (clicking) {
+    textbox.value += "e";
+  }
+}
+function rButton() {
+  if (clicking) {
+    textbox.value += "r";
+  }
+}
+function tButton() {
+  if (clicking) {
+    textbox.value += "t";
+  }
+}
+function yButton() {
+  if (clicking) {
+    if (word5) {
+      textArray += "is";
+      textbox.value = textArray;
+    } else {
+      textbox.value += "y";
+    }
+  }
+}
+function uButton() {
+  if (clicking) {
+    textbox.value += "u";
+  }
+}
+function iButton() {
+  if (clicking) {
+    textbox.value += "i";
+  }
+}
+function oButton() {
+  if (clicking) {
+    textbox.value += "o";
+  }
+}
+function pButton() {
+  if (clicking) {
+    textbox.value += "p";
+  }
+}
+
+//second row
+function aButton() {
+  if (clicking) {
+    textbox.value += "a";
+  }
+}
+function sButton() {
+  if (clicking) {
+    if (word5) {
+      textArray += "this";
+      textbox.value = textArray;
+    } else {
+      textbox.value += "s";
+    }
+  }
+}
+function dButton() {
+  if (clicking) {
+    textbox.value += "d";
+  }
+}
+function fButton() {
+  if (clicking) {
+    textbox.value += "f";
+  }
+}
+function gButton() {
+  if (clicking) {
+    textbox.value += "g";
+  }
+}
+function hButton() {
+  if (clicking) {
+    textbox.value += "h";
+  }
+}
+function jButton() {
+  if (clicking) {
+    textbox.value += "j";
+  }
+}
+function kButton() {
+  if (clicking) {
+    textbox.value += "k";
+  }
+}
+function lButton() {
+  if (clicking) {
+    textbox.value += "l";
+  }
+}
+
+//bottom row
+function zButton() {
+  if (clicking) {
+    textbox.value += "z";
+  }
+}
+function xButton() {
+  if (clicking) {
+    textbox.value += "x";
+  }
+}
+function cButton() {
+  if (clicking) {
+    textbox.value += "c";
+  }
+}
+function vButton() {
+  if (clicking) {
+    textbox.value += "v";
+  }
+}
+function bButton() {
+  if (clicking) {
+    if (word5) {
+      textArray += "cool?";
+      textbox.value = textArray;
+    } else {
+      textbox.value += "b";
+    }
+  }
+}
+function nButton() {
+  if (clicking) {
+    textbox.value += "n";
+  }
+}
+function mButton() {
+  if (clicking) {
+    textbox.value += "m";
+  }
+}
+function spaceButton() {
+  if (clicking) {
+    if (word5) {
+      textArray += " ";
+      textbox.value = textArray;
+    } else {
+      textbox.value += " ";
+    }
+  }
+}
+
+function periodButton() {
+  if (clicking) {
+    if (word5) {
+      textArray += ".";
+      textbox.value = textArray;
+    } else {
+      textbox.value += ".";
+    }
+  }
+}
---
+++ main.js
@@ -0,0 +1,83 @@
+//varibles
+let h3 = document.getElementsByTagName("h3");
+let textArray = [];
+const text1 = "hey, how's it going";
+const text2 = "i'm so glad you decided to play my typing game";
+const text3 =
+  "it means a lot to me that you'd play my totally ordinary typing game with nothing special about it";
+const text4 =
+  "that looked rough. you deserve a break. you can just click on the keys now";
+const text5 = "is this cool?";
+const text6 = "hope you had fun, goodbye!";
+let word1 = true;
+let word2 = false;
+let word3 = false;
+let word4 = false;
+let word5 = false;
+let word6 = false;
+let clicking = false;
+
+//this is the intital word set
+function topText() {
+  h3[0].innerText = text1;
+}
+
+//reset button
+reset.addEventListener("click", resetButton);
+
+function resetButton() {
+  textbox.value = "";
+  textArray = [];
+}
+
+//submit button
+submit.addEventListener("click", submitButton);
+
+function submitButton() {
+  const help = textbox.value.toString();
+  const help1 = h3[0].innerText.toString();
+  if (help == help1) {
+    textArray = [];
+    textbox.value = "";
+    if (word1) {
+      h3[0].innerText = text2;
+      word1 = false;
+      word2 = true;
+      change1();
+    } else if (word2) {
+      h3[0].innerText = text3;
+      word2 = false;
+      word3 = true;
+      change2();
+    } else if (word3) {
+      h3[0].innerText = text4;
+      word3 = false;
+      word4 = true;
+      change3();
+      clicking = true;
+    } else if (word4) {
+      h3[0].innerText = text5;
+      word4 = false;
+      word5 = true;
+      change4();
+      clicking = true;
+    } else if (word5) {
+      h3[0].innerText = text6;
+      word5 = false;
+      word6 = true;
+      change5();
+      clicking = false;
+    } else if (word6) {
+      alert("Congratulations! You've won");
+      if (alert) {
+        location.reload();
+      }
+    }
+  } else {
+    textbox.value = "";
+    textArray = [];
+    alert(
+      "Whoops, you messed up! That's ok, you'll get it for sure this time. Everybody makes mistakes, try again :)"
+    );
+  }
+}
